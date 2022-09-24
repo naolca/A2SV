223c57e333bc0291bbc35463964f80dc95e8af1c
naolca <95067271+naolca@users.noreply.github.com>
2022-09-24 19:16:26 +0300

    Time: 63 ms (16.57%), Space: 13.9 MB (11.64%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.py
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.py
@@ -5,10 +5,18 @@
 #         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        the_list_from_the_middle_Node=[]
-        while head!=None:
-            the_list_from_the_middle_Node.append(head)
-            head=head.next
-        return the_list_from_the_middle_Node[len(the_list_from_the_middle_Node)//2]
-            
+         if not head:
+            return
+         length = 0
+         node = head
         
+         while node:#until the node is the tail
+                
+                node = node.next
+                length += 1
+            
+         for i in range(length // 2):
+            
+            head = head.next#this loop ends when the head has the value of the middle element
+        
+         return head
