92847d63dd257d1d544aed60998d4f29c5672bd6
naolca <95067271+naolca@users.noreply.github.com>
2022-09-29 08:49:21 +0300

    Time: 445 ms (5.09%), Space: 18 MB (84.83%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.py
@@ -0,0 +1,12 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        i=0
+        intervals.sort()
+        while i< len(intervals)-1:
+            if intervals[i][1]>=intervals[i+1][0]:
+                intervals[i][1] = max(intervals[i][1],intervals[i+1][1] )
+                intervals.remove(intervals[i+1])
+            else:
+                i+=1
+        return intervals
+        
