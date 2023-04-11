7d33b8cba678d2325d7c97ec1b6775649896544e
Firaol Ibrahim <95067271+naolca@users.noreply.github.com>
2023-04-11 19:34:21 +0300

    added construct-string-from-binary-tree.python

---
+++ construct-string-from-binary-tree.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def tree2str(self, root: Optional[TreeNode]) -> str:
+
+        def dfs(node):
+            if not node:
+                return "()"
+            if node.left and node.right:
+                return str(node.val)+'('+dfs(node.left)+')('+dfs(node.right)+')'
+            elif not node.left and node.right:
+                return str(node.val)+'()('+dfs(node.right)+')'
+            elif not node.right and node.left:
+                return str(node.val)+'('+dfs(node.left)+')'
+            else:
+                return str(node.val)
+        return dfs(root)
